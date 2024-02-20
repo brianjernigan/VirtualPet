807e027a778f7e30f7a9506b15bf886551959b6c
brianjernigan <brobertjernigan@gmail.com>
2024-02-20 18:42:42 -0500

    Pet Class first commit
    Wrote pet class

--- Assets/Scripts/Pet.cs
+++ Assets/Scripts/Pet.cs
@@ -2,7 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class Pet
 {
-    
+    private string _name;
+    private int _hunger;
+    private int _boredom;
+    private int _fatigue;
+
+    public string Name
+    {
+        get => _name;
+        set => _name = value;
+    }
+
+    public int Hunger
+    {
+        get => _hunger;
+        set => _hunger = value;
+    }
+
+    public int Boredom
+    {
+        get => _boredom;
+        set => _boredom = value;
+    }
+
+    public int Fatigue
+    {
+        get => _fatigue;
+        set => _fatigue = value;
+    }
+
+    public void Eat(int cals)
+    {
+        _hunger -= cals;
+    }
+
+    public void Play(int exercise)
+    {
+        _boredom -= exercise;
+    }
+
+    public void Rest(int sleep)
+    {
+        _fatigue -= sleep;
+    }
 }
